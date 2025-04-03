92cc03a49665a2bf26daf49d126d73889f4ab17b
MiguelGavito <mgavitogzz@gmail.com>
2025-04-03 07:34:24 -0600

    codipo pendiente de push

--- index.ts
+++ index.ts
@@ -3,11 +3,11 @@ import tweetsRoutes from './src/routes/tweets';
 import userRoutes from './src/routes/user';
 
 const app = express();
-
 app.use(express.json());
+
 app.use('/api/tweets', tweetsRoutes);
 app.use('/api/users', userRoutes);
 
 
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => console.log('Server running'))
+app.listen(PORT, () => console.log('Server running'));
--- src/controllers/tweet.ts
+++ src/controllers/tweet.ts
@@ -1,12 +1,17 @@
 import TweetDataBase from "../db/tweet";
+import { getNumbersFromDB } from '../db/db';
 
 const tweetDataBase = new TweetDataBase();
 
 class TweetController {
-
-  async getAllTweets(){
-    return await tweetDataBase.getAllTweets();
+  async getAllTweets(): Promise<number[]> {
+    const numbers = await tweetDataBase.getAllTweets();
+    
+    const evenNumbers = numbers.filter(num => num % 2 === 0);
+    return evenNumbers;
   }
+
+  
 }
 
 export default TweetController;
